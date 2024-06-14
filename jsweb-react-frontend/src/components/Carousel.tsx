ddf1290c89bd7d88476e4624984af8af53dd2840
Josh S <Joshua.Stevens@cubesys.com.au>
2024-06-14 12:56:37 +1000

    added carousel for home page

---
+++ jsweb-react-frontend/src/components/Carousel.tsx
@@ -0,0 +1,109 @@
+import React, { useState, useEffect } from "react";
+import { Box, Icon } from "@mui/material";
+import { IconButton } from "@mui/material";
+import Slide from "@mui/material/Slide";
+import Stack from "@mui/material/Stack";
+import { HomeTitleCard } from "./HomeTitleCard";
+import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+
+/** ------------------------------------
+ *  Carousel
+ *  ------------------------------------
+ *  Component to render a dynamic slideshow of pages.
+ *
+ *  React, useState, useEffect: These are fundamental hooks from React. useState lets us maintain state in our component, and useEffect allows us to run side-effects (like fetching data or updating the DOM).
+ *  Followed the following page for guidance: https://medium.com/@ltomblock/crafting-a-professional-looking-carousel-with-react-and-mui-746a86af0ab0
+ * ------------------------------------ */
+
+export const Carousel = () => {
+  const [cards, setCards] = useState<React.ReactElement[]>([]);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [slideDirection, setSlideDirection] = useState<
+    "right" | "left" | undefined
+  >("left");
+
+  const cardsPerPage = 1; //defines how many cards show per page
+  const duplicateCards: React.ReactElement[] = Array.from(
+    { length: 10 },
+    (_, i) => (
+      <HomeTitleCard
+        title="Title"
+        subtitle="Subtitle"
+        buttonLink=""
+        buttonText="button"
+        themecolor="pink"
+        buttonvisible={false}
+        key={i}
+      />
+    )
+  );
+
+  const handleNextPage = () => {
+    setSlideDirection("left");
+    setCurrentPage((prevPage) => prevPage + 1);
+  };
+
+  const handlePrevPage = () => {
+    setSlideDirection("right");
+    setCurrentPage((prevPage) => prevPage - 1);
+  };
+
+  useEffect(() => {
+    setCards(duplicateCards);
+  }, []); //Here, we’re using the useEffect hook to initially set our cards to the duplicateCards array when the component mounts. The empty array [] means this effect runs only once, similar to componentDidMount in class components. When creating a more dynamic site, which we will cover in another tutorial, this would most be linked to some sort of call to collect the relevant data and then set the state of the “cards” array.
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        alignContent: "center",
+        justifyContent: "center",
+        height: "400px",
+      }}
+    >
+      <IconButton
+        onClick={handlePrevPage}
+        sx={{ margin: 5 }}
+        disabled={currentPage == 0}
+      >
+        <NavigateBeforeIcon />
+      </IconButton>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          alignContent: "center",
+          justifyContent: "center",
+          height: "400px",
+        }}
+      >
+        {cards.map((card, index) => (
+          <Box
+            key={"card-${index}"}
+            sx={{
+              width: "100%",
+              hieght: "100%",
+              display: currentPage === index ? "block" : "none",
+            }}
+          >
+            <Slide direction={slideDirection} in={currentPage===index}>
+                <Stack spacing={2} direction="row"> {cards.slice(index*cardsPerPage, index*cardsPerPage+cardsPerPage)}</Stack>
+            </Slide>
+
+          </Box>
+        ))}
+      </Box>
+      <IconButton
+        onClick={handleNextPage}
+        sx={{ margin: 5 }}
+        disabled={currentPage >= Math.ceil((cards.length || 0)/cardsPerPage)-1}
+      >
+        <NavigateNextIcon />
+      </IconButton>
+    </Box>
+  );
+};
--- jsweb-react-frontend/src/views/Landing/Home.tsx
+++ jsweb-react-frontend/src/views/Landing/Home.tsx
@@ -1,12 +1,14 @@
 import "../../styling/views.css";
 import "../../styling/style.css";
 import { Grid } from "@mui/material";
+import { Box } from "@mui/material";
 
 //subpage content
 import { PortfolioSummary } from "./PortfolioSubpage";
 import { AboutSubpageContent } from "./AboutSubpage";
 
 import { FormRow } from "../../components/FormRow";
+import { Carousel } from "../../components/Carousel";
 /** ------------------------------------
  *  Home
  *  ------------------------------------
@@ -20,51 +22,8 @@ const portfoliosummary = PortfolioSummary();
 
 export const Home = () => {
   return (
-    <Grid container spacing={1} sx={{ flexGrow: 1 }}>
-      <Grid container item className="HomeGrid-Row">
-        <FormRow
-          title="Joshua-Stevens.Me"
-          subtitle="Welcome to my personal website!"
-          button_include={false}
-          button_text=""
-          button_link=""
-          theme_color="primary"
-          subcontent={""}
-        />
-      </Grid>
-      <Grid container item className="HomeGrid-Row">
-        <FormRow
-          title="About"
-          subtitle="Welcome to my personal website!"
-          button_include={true}
-          button_text="about"
-          button_link="about"
-          theme_color="#323F49"
-          subcontent={AboutSubpageContent()}
-        />
-      </Grid>
-      <Grid container item className="HomeGrid-Row">
-        <FormRow
-          title="Portfolio"
-          subtitle="Welcome to my personal website!"
-          button_include={true}
-          button_text="Portfolio"
-          button_link="portfolio"
-          theme_color="#D85A62"
-          subcontent={PortfolioSummary()}
-        />
-      </Grid>
-      <Grid container item className="HomeGrid-Row">
-        <FormRow
-          title="Blog"
-          subtitle="Welcome to my personal website!"
-          button_include={true}
-          button_text="Blog"
-          button_link="blog"
-          theme_color="#6AB97B"
-          subcontent={portfoliosummary}
-        />
-      </Grid>
-    </Grid>
+    <>
+        <Carousel></Carousel>
+    </>
   );
 };
